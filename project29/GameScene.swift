1ba9ee6a4cf228112f3ccb0417ab3c7696c8d7d2
Zeemoni <gaboahene1@gmail.com>
2022-06-16 02:15:15 +0000

    first commit

---
+++ project29/GameScene.swift
@@ -0,0 +1,317 @@
+//
+//  GameScene.swift
+//  project29
+//
+//  Created by Olibo moni on 07/03/2022.
+//
+
+import SpriteKit
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case player = 4
+    case building = 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+   var buildings = [BuildingNode]()
+    weak var controller: GameViewController?
+    
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var banana: SKSpriteNode!
+    var restartLabel: SKLabelNode!
+    var gameOver: SKLabelNode!
+    var isGameOver = false
+    
+    
+     var player1Score = 0
+    
+    
+    var player2Score = 0
+//    {
+//        didSet {
+//            controller?.scoreLabel.text = "Score: \(player2Score)"
+//            controller?.scoreLabel.text = "Score: \(player1Score)"
+//        }
+//    }
+    
+    
+    var currentPlayer = 1
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        createBuildings()
+        createPlayers()
+        physicsWorld.contactDelegate = self
+        
+        let pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(recognizer: )))
+        self.view?.addGestureRecognizer(pinchRecognizer)
+  
+        
+    }
+    
+    func createBuildings(){
+       
+        var currentX: CGFloat = -15
+
+            while currentX < 1024 {
+                let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+                currentX += size.width + 2
+
+                let building = BuildingNode(color: UIColor.red, size: size)
+                building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+                building.setup()
+                addChild(building)
+
+                buildings.append(building)
+            }
+    }
+    
+    func launch(angle: Int, velocity: Int){
+        guard !isGameOver else { return }
+        let speed = Double(velocity) / 10
+        let radians = deg2rad(degrees: angle)
+        
+        if banana != nil {
+            banana.removeFromParent()
+            banana = nil
+        }
+        
+        banana = SKSpriteNode(imageNamed: "banana")
+           banana.name = "banana"
+           banana.physicsBody = SKPhysicsBody(circleOfRadius: banana.size.width / 2)
+           banana.physicsBody?.categoryBitMask = CollisionTypes.banana.rawValue
+           banana.physicsBody?.collisionBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+           banana.physicsBody?.contactTestBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+           banana.physicsBody?.usesPreciseCollisionDetection = true
+           addChild(banana)
+        
+        if currentPlayer == 1 {
+            banana.position = CGPoint(x: player1.position.x - 30, y: player1.position.y + 40)
+            banana.physicsBody?.angularVelocity = -20
+            
+            let raisedArm = SKAction.setTexture(SKTexture(imageNamed: "player1Throw"))
+            let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+            let pause = SKAction.wait(forDuration: 0.15)
+            let sequence = SKAction.sequence([raisedArm, pause, lowerArm])
+            player1.run(sequence)
+            
+            let impulse = CGVector(dx: cos(radians) * speed, dy: sin(radians) * speed)
+            banana.physicsBody?.applyImpulse(impulse)
+        } else {
+            banana.position = CGPoint(x: player2.position.x + 30, y: player2.position.y + 40)
+            banana.physicsBody?.angularVelocity = 20
+            
+            let raisedArm = SKAction.setTexture(SKTexture(imageNamed: "player2Throw"))
+            let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+            let pause = SKAction.wait(forDuration: 0.15)
+            let sequence = SKAction.sequence([raisedArm, pause, lowerArm])
+            player2.run(sequence)
+            
+            let impulse = CGVector(dx: cos(radians) * -speed, dy: sin(radians) * speed)
+            banana.physicsBody?.applyImpulse(impulse)
+        }
+    }
+    
+    func createPlayers(){
+        guard !isGameOver else { return}
+        player1 = SKSpriteNode(imageNamed: "player")
+        player1.name = "player1"
+        player1.physicsBody = SKPhysicsBody(circleOfRadius: player1.size.width / 2)
+        player1.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player1.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.isDynamic = false
+        
+        
+        let player1Building = buildings[1]
+        player1.position = CGPoint(x: player1Building.position.x, y: player1Building.position.y + ((player1Building.size.height + player1.size.height) / 2))
+        addChild(player1)
+        
+        player2 = SKSpriteNode(imageNamed: "player")
+        player2.name = "player2"
+        player2.physicsBody = SKPhysicsBody(circleOfRadius: player2.size.width / 2)
+        player2.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player2.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.isDynamic = false
+        
+        
+        let player2Building = buildings[buildings.count - 2]
+        player2.position = CGPoint(x: player2Building.position.x, y: player2Building.position.y + ((player2Building.size.height + player2.size.height) / 2))
+        addChild(player2)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        if currentPlayer == 1 {
+            controller?.scoreLabel.text = "Score: \(player1Score)"
+        } else {
+            controller?.scoreLabel.text = "Score: \(player2Score)"
+        }
+        
+        guard banana != nil else { return }
+        
+        if abs(banana.position.y) > 1000 {
+            banana.removeFromParent()
+            banana = nil
+            changePlayer()
+            
+        }
+    }
+    
+    func deg2rad(degrees: Int) ->Double {
+        return Double(degrees) * .pi / 180
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody: SKPhysicsBody
+        let secondBody: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        guard let firstNode = firstBody.node else { return }
+        guard let secondNode = secondBody.node else { return }
+        
+        if firstNode.name == "banana" && secondNode.name == "building" {
+            bananaHit(building: secondNode, atPoint: contact.contactPoint)
+        }
+        
+        if firstNode.name == "banana" && secondNode.name == "player1" {
+            destroy(player: player1)
+            player2Score += 1
+            
+        }
+        
+        if firstNode.name == "banana" && secondNode.name == "player2" {
+            destroy(player: player2)
+            player1Score += 1
+        }
+        
+        if player1Score  > 2  {
+            endGame(player: "Player One")
+        } else if player2Score > 2 {
+            endGame(player: "Player Two")
+        }
+        
+    }
+    
+    func destroy(player: SKSpriteNode){
+        if let explosion = SKEmitterNode(fileNamed: "hitPlayer") {
+            explosion.position = player.position
+            addChild(explosion)
+        }
+        player.removeFromParent()
+        banana.removeFromParent()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [self] in
+            let newGame = GameScene(size: self.size)
+            newGame.controller = self.controller
+            self.controller?.currentGame = newGame
+            
+            self.changePlayer()
+            
+            newGame.currentPlayer = self.currentPlayer
+            newGame.player1Score = self.player1Score
+            newGame.player2Score = self.player2Score
+        
+            
+            let transition = SKTransition.doorway(withDuration: 1.5)
+            
+            guard !isGameOver else { return}
+            self.view?.presentScene(newGame, transition: transition)
+               
+            
+           
+            
+        }
+    }
+    
+    func bananaHit(building: SKNode, atPoint contactPoint: CGPoint){
+        guard let building = building as? BuildingNode else { return }
+        let buildingLocation = convert(contactPoint, to: building)
+        building.hit(at: buildingLocation)
+        
+        if let explosion = SKEmitterNode(fileNamed: "hitBuilding") {
+            explosion.position = contactPoint
+            addChild(explosion)
+        }
+        
+        banana.name = ""
+        banana.removeFromParent()
+        banana = nil
+        changePlayer()
+        
+    }
+    
+    
+    
+    func changePlayer(){
+        guard !isGameOver else { return}
+        if currentPlayer == 1 {
+            currentPlayer = 2
+        } else {
+            currentPlayer = 1
+        }
+        controller?.activatePlayer(number: currentPlayer)
+    }
+    
+    
+    
+    func endGame(player: String){
+        
+        gameOver = SKLabelNode(text: "\(player) wins")
+        gameOver.fontSize = 50
+        gameOver.position = CGPoint(x: 512, y: 384)
+        gameOver.fontName = "Chalkduster"
+        gameOver.zPosition = 1
+        addChild(gameOver)
+        
+        isGameOver = true
+        restartLabel = SKLabelNode(text: "Pinch to Restart")
+        restartLabel.fontSize = 50
+        restartLabel.position = CGPoint(x: 512 , y: gameOver.position.y + 200)
+        restartLabel.fontName = "Chalkduster"
+        restartLabel.zPosition = 1
+        restartLabel.fontColor = .red
+        addChild(restartLabel)
+    }
+    
+    func didEnd(_ contact: SKPhysicsContact) {
+        if currentPlayer == 1 {
+            controller?.scoreLabel.text = "Score: \(player1Score)"
+        } else {
+            controller?.scoreLabel.text = "Score: \(player2Score)"
+        }
+    }
+    
+    func restartGame(){
+        guard isGameOver else { return}
+        isGameOver = false
+       // DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            let newGame = GameScene(size: self.size)
+            newGame.controller = self.controller
+            self.controller?.currentGame = newGame
+            newGame.player1Score = 0
+            newGame.player2Score = 0
+            let transition = SKTransition.crossFade(withDuration: 1.5)
+            self.view?.presentScene(newGame, transition: transition)
+            
+            
+        //}
+    }
+    
+    @objc func handlePinch(recognizer: UIPinchGestureRecognizer){
+        if recognizer.state == .ended {
+            restartGame()
+        }
+    }
+    
+}
---
+++ project29/GameViewController.swift
@@ -0,0 +1,109 @@
+//
+//  GameViewController.swift
+//  project29
+//
+//  Created by Olibo moni on 07/03/2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    var currentGame: GameScene?
+    
+    @IBOutlet var angleSlider: UISlider!
+    @IBOutlet var angleLabel: UILabel!
+    @IBOutlet var velocitySlider: UISlider!
+    @IBOutlet var velocityLabel: UILabel!
+    @IBOutlet var launchButton: UIButton!
+    @IBOutlet var playerNumber: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .fill
+                
+                // Present the scene
+                view.presentScene(scene)
+                
+                currentGame = scene as? GameScene
+                currentGame?.controller = self 
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+        
+        angleChanged(self)
+        velocityChanged(self)
+        
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBAction func angleChanged(_ sender: Any) {
+        angleLabel.text = "Angle: \(Int(angleSlider.value))°"
+    }
+    
+    @IBAction func velocityChanged(_ sender: Any) {
+        velocityLabel.text = "Velocity: \(Int(velocitySlider.value))"
+    }
+    
+    @IBAction func launch(_ sender: UIButton) {
+        angleSlider.isHidden = true
+        angleLabel.isHidden = true
+        
+        velocitySlider.isHidden = true
+        velocityLabel.isHidden = true
+        
+        launchButton.isHidden = true
+        
+        currentGame?.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
+    }
+    
+    func activatePlayer(number: Int){
+        if number == 1 {
+            playerNumber.text = "<<< PLAYER ONE"
+            //scoreLabel.text = "Score \(currentGame!.player1Score)"
+            
+        } else {
+            playerNumber.text = "PLAYER TWO >>>"
+            //scoreLabel.text = "Score \(currentGame!.player2Score)"
+        }
+        
+        angleSlider.isHidden = false
+        angleLabel.isHidden = false
+        
+        velocitySlider.isHidden = false
+        velocityLabel.isHidden = false
+        
+        launchButton.isHidden = false
+    }
+    
+}
